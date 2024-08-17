690b8ce367ac5bfa9e9ec1015351647806215a63
kyrylosheliah <kyrylosheliah@gmail.com>
2024-08-17 08:29:35 +0300

    rewrite incoming, deleted old fs

--- lua/config/init.lua
+++
@@ -1,4 +0,0 @@
-require "config.general"
-require "config.keymap"
-require "config.statusline"
-require "config.transparency"
--- lua/plugins/cmpsource/1.codeium.lua
+++
@@ -1,9 +0,0 @@
-return {
-  enabled = false,
-  "Exafunction/codeium.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
-  },
-  opts = {},
-}
