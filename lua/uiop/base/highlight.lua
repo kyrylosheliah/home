623b3bc8dbb2bba38cccf4843095e27878269a6a
kyrylosheliah <kyrylosheliah@gmail.com>
2024-12-17 21:01:50 +0200

    17.12.2024

--- lua/uiop/base/highlight.lua
+++ lua/uiop/base/highlight.lua
@@ -1,7 +1,8 @@
 -- thanks to "mellow-theme/mellow.nvim" and "datsfilipe/nvim-colorscheme-template"
 
 vim.g.debug = true
-vim.g.transparent = false
+--vim.g.transparent = false
+vim.g.transparent = true
 --vim.g.highlight_table_name = "visual_studio"
 vim.g.highlight_table_name = "custom"
 --vim.g.highlight_table_name = "mix"
