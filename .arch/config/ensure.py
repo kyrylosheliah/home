61f86cace761db893637a7c37db5ff15150b4d22
kyrylosheliah <kyrylosheliah@gmail.com>
2025-06-09 15:49:55 +0300

    remove bash implementation; refactor and update subprocess api use in python implementation

--- .arch/config/ensure.py
+++ .arch/config/ensure.py
@@ -11,136 +11,169 @@ from libensure import (
     user_services_enabled,
     files_contents,
     log_error,
-    log_success,
 )
 
-def main():
-    username = os.getlogin()
-    hostname = socket.gethostname()
+if __name__ != '__main__':
+    log_error("This script is not a library")
+    sys.exit(1)
 
-    ensurance_result = ensure([
+if os.getuid() == 0:
+    log_error("This script should not be run as a root or with sudo")
+    sys.exit(1)
 
-        # network
-        { "ensure": packages_installed, "for": [
-            'networkmanager',
-            ] },
-        { "ensure": system_services_enabled, "for": [
-            'NetworkManager.service',
-            ] },
+username = os.getlogin()
+hostname = socket.gethostname()
 
-        # sound
-        { "ensure": packages_installed, "for": [
-            "pipewire",
-            "pipewire-alsa",
-            "pipewire-jack",
-            "pipewire-pulse",
-            ] },
-        { "ensure": user_services_enabled, "for": [
-            'pipewire-pulse.service',
-            ] },
+sys.exit(1 if False == ensure([
 
-        # base
-        { "ensure": packages_installed, "for": [
-            'base-devel',
-            'htop',
-            'wget',
-            'curl',
-            'unzip',
-            'man-db',
-            ] },
+    # network
+    { "ensure": packages_installed, "for": [
+        'networkmanager',
+        ] },
+    { "ensure": system_services_enabled, "for": [
+        'NetworkManager.service',
+        ] },
 
-        # git
-        { "ensure": packages_installed, "for": [
-            'git',
-            'less',
-            ] },
+    # bluetooth
+    { "ensure": packages_installed, "for": [
+        "bluez", "bluez-utils"
+        ] },
+    { "ensure": system_services_enabled, "for": [
+        "bluetooth.service",
+        ] },
 
-        # desktop
-        { "ensure": packages_installed, "for": [
-            # fonts
-            "noto-fonts",
-            "noto-fonts-emoji",
-            "ttf-iosevka-nerd",
-            # ui
-            "hyprland",
-            "hyprpaper",
-            "waybar",
-            "rofi-wayland",
-            ] },
+    # audio
+    { "ensure": packages_installed, "for": [
+        "pipewire",
+        "pipewire-alsa",
+        "pipewire-jack",
+        "pipewire-pulse",
+        ] },
+    { "ensure": user_services_enabled, "for": [
+        'pipewire-pulse.service',
+        ] },
 
-        # development
-        { "ensure": packages_installed, "for": [
-            #"python",
-            "neovim", # laptop kb baaaaad
-            "wl-clipboard", # neovim to linux and vice versa clipboard support
-            ] },
-        { "ensure": aur_packages_installed, "for": [
-            "code",
-            ] },
+    # storage
+    { "ensure": system_services_enabled, "for": [
+        "fstrim.timer", # Enable TRIM for SSDs
+        ] },
 
-        # graphics
-        { "ensure": files_contents, "for": [
-            # multilib repository
-            { "filename": "/etc/pacman.conf", "contents": """
+    # base
+    { "ensure": packages_installed, "for": [
+        'base-devel',
+        'htop',
+        'wget',
+        'curl',
+        'unzip',
+        'man-db',
+        ] },
+
+    # git
+    { "ensure": packages_installed, "for": [
+        'git',
+        'less',
+        ] },
+
+    # desktop
+    { "ensure": packages_installed, "for": [
+        # fonts
+        "noto-fonts",
+        "noto-fonts-emoji",
+        "ttf-iosevka-nerd",
+        # ui
+        "hyprland",
+        "hyprpaper",
+        "waybar",
+        "rofi-wayland",
+        ] },
+
+    # development
+    { "ensure": packages_installed, "for": [
+        #"python",
+        "neovim", # laptop kb baaaaad
+        "wl-clipboard", # neovim to linux and vice versa clipboard support
+        ] },
+    { "ensure": aur_packages_installed, "for": [
+        "code",
+        ] },
+
+    # graphics
+    { "ensure": files_contents, "for": [
+        # multilib repository
+        { "filename": "/etc/pacman.conf", "contents": """
 [multilib]
 Include = /etc/pacman.d/mirrorlist
-                """ },
-            ] },
-        # amd graphics
-        { "ensure": packages_installed, "for": [
-                "mesa", "lib32-mesa", "vulkan-radeon", "lib32-vulkan-radeon",
-                "vulkan-icd-loader", "lib32-vulkan-icd-loader",
-            ] },
-        { "ensure": aur_packages_installed, "for": [
-                "lact",
-            ] },
-        # nvidia graphics
-        #{ "ensure": packages_installed, "for": [
-        #    "nvidia", "nvidia-utils", "lib32-nvidia-utils", "nvidia-settings",
-        #    "opencl-nvidia",
-        #    ] },
-        # TODO: EITHER dispatch accumulation per hostname OR make an addtional
-        # detailed hardware configuration `*.py` imported interface
+            """ },
+        ] },
+    # amd graphics
+    { "ensure": packages_installed, "for": [
+        "mesa", "lib32-mesa", "vulkan-radeon", "lib32-vulkan-radeon",
+        "vulkan-icd-loader", "lib32-vulkan-icd-loader",
+        ] },
+    { "ensure": aur_packages_installed, "for": [
+        "lact",
+        ] },
+    # nvidia graphics
+    #{ "ensure": packages_installed, "for": [
+    #    "nvidia", "nvidia-utils", "lib32-nvidia-utils", "nvidia-settings",
+    #    "opencl-nvidia",
+    #    ] },
+    # TODO: EITHER dispatch accumulation per hostname OR make an addtional
+    # detailed hardware configuration `*.py` imported interface
 
-        # gaming
-        { "ensure": system_services_enabled, "for": [
-            "bluetooth.service",
-            "fstrim.timer", # Enable TRIM for SSDs
-            ] },
-        { "ensure": packages_installed, "for": [
-            "steam", "lutris", "wine-staging", "winetricks", "gamemode",
-            "lib32-gamemode", "giflib", "lib32-giflib", "libpng",
-            "lib32-libpng", "libldap", "lib32-libldap", "gnutls",
-            "lib32-gnutls", "mpg123", "lib32-mpg123", "openal", "lib32-openal",
-            "v4l-utils", "lib32-v4l-utils", "libgpg-error",
-            "lib32-libgpg-error", "alsa-plugins", "lib32-alsa-plugins",
-            "alsa-lib", "lib32-alsa-lib", "libjpeg-turbo",
-            "lib32-libjpeg-turbo", "sqlite", "lib32-sqlite", "libxcomposite",
-            "lib32-libxcomposite", "libxinerama", "lib32-libxinerama",
-            "ncurses", "lib32-ncurses", "opencl-icd-loader",
-            "lib32-opencl-icd-loader", "libxslt", "lib32-libxslt", "libva",
-            "lib32-libva", "gtk3", "lib32-gtk3", "gst-plugins-base-libs",
-            "lib32-gst-plugins-base-libs", "vulkan-icd-loader",
-            "lib32-vulkan-icd-loader", "obs-studio", "discord", "mangohud",
-            "lib32-mangohud", "goverlay", "gamescope", "solaar", "bluez",
-            "bluez-utils", "lib32-libpulse", "pipewire", "pipewire-pulse",
-            "pipewire-alsa", "linux-headers", "xwaylandvideobridge",
-            ] },
-        { "ensure": aur_packages_installed, "for": [
-            "vkbasalt", "lib32-vkbasalt", "proton-ge-custom-bin",
-            "xone-dkms-git", "dxvk-bin", "vkd3d-proton-bin",
-            ] },
+    ## gaming
+    ## launchers
+    #{ "ensure": packages_installed, "for": [
+    #    "steam", "lutris", "gamescope",
+    #    ] },
+    ## windows compatibility
+    #{ "ensure": packages_installed, "for": [
+    #    "wine-staging", "winetricks",
+    #    "proton-ge-custom-bin", "dxvk-bin",
+    #    "vkd3d-proton-bin",
+    #    ] },
+    ## low-level features or driver compatibility
+    #{ "ensure": packages_installed, "for": [
+    #    "linux-headers", "solaar", "xwaylandvideobridge", "xone-dkms-git"
+    #    ] },
+    #{ "ensure": packages_installed, "for": [
+    #    # native and 32 bit compatibility libraries
+    #    "giflib", "lib32-giflib",
+    #    "libpng", "lib32-libpng",
+    #    "libldap", "lib32-libldap",
+    #    "gnutls", "lib32-gnutls",
+    #    "mpg123", "lib32-mpg123",
+    #    "openal", "lib32-openal",
+    #    "v4l-utils", "lib32-v4l-utils",
+    #    "libgpg-error", "lib32-libgpg-error",
+    #    "alsa-plugins", "lib32-alsa-plugins",
+    #    "sqlite", "lib32-sqlite",
+    #    "alsa-lib", "lib32-alsa-lib",
+    #    "libjpeg-turbo", "lib32-libjpeg-turbo",
+    #    "libxcomposite", "lib32-libxcomposite",
+    #    "libxinerama", "lib32-libxinerama",
+    #    "ncurses", "lib32-ncurses",
+    #    "opencl-icd-loader", "lib32-opencl-icd-loader",
+    #    "libxslt", "lib32-libxslt",
+    #    "libva", "lib32-libva",
+    #    "gtk3", "lib32-gtk3",
+    #    "gst-plugins-base-libs", "lib32-gst-plugins-base-libs",
+    #    "vulkan-icd-loader", "lib32-vulkan-icd-loader",
+    #    "libpulse", "lib32-libpulse",
+    #    # performance
+    #    "gamemode", "lib32-gamemode",
+    #    "mangohud", "lib32-mangohud",
+    #    "vkbasalt", "lib32-vkbasalt"
+    #    ] },
 
-        ]) # END ensure()
+    # apps
+    { "ensure": packages_installed, "for": [
+        # recording
+        #"obs-studio", "goverlay"
+        # apps
+        "discord",
+        "firefox",
+        ] },
 
-    if ensurance_result == False:
-        log_error("An error occurred during configuration check, exiting")
-        sys.exit(1)
-    log_success("Configuration check success")
-
-if __name__ == '__main__':
-    if os.getuid() == 0:
-        log_error("This script should not be run as a root or with sudo")
-        sys.exit(1)
-    main()
+]) else 0) # END ensure()
 
--- .arch/config/libensure.py
+++ .arch/config/libensure.py
@@ -5,8 +5,6 @@
 #        "",
 #        "",
 #        ] },
-#    { "ensure": aur_packages_installed, "for": [ "", "", "" ] },
-#    { "ensure": services_enabled, "for": [ "", "", "" ] },
 #    { "ensure": files_contents, "for": [
 #        {
 #          "filename": "",
@@ -37,56 +35,60 @@ class bcolors:
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
 
+log_prefix = f"{bcolors.DIM}ensure:{bcolors.ENDC} "
+
 def log_error(message: str):
-    print(f"ensure.py | {bcolors.RED}{bcolors.BOLD}{message}{bcolors.ENDC}")
+    print(f"{log_prefix}{bcolors.RED}{bcolors.BOLD}{message}{bcolors.ENDC}")
 
 def log_success(message: str):
-    print(f"ensure.py | {bcolors.GREEN}{bcolors.BOLD}{message}{bcolors.ENDC}")
+    print(f"{log_prefix}{bcolors.GREEN}{bcolors.BOLD}{message}{bcolors.ENDC}")
 
 def log_warning(message: str):
-    print(f"ensure.py | {bcolors.YELLOW}{message}{bcolors.ENDC}")
+    print(f"{log_prefix}{bcolors.YELLOW}{message}{bcolors.ENDC}")
 
 def log_info(message: str):
-    print(f"ensure.py | {bcolors.DIM}{message}{bcolors.ENDC}")
+    print(f"{log_prefix}{bcolors.DIM}{message}{bcolors.ENDC}")
 
-def sh(command: List[str]) -> subprocess.CompletedProcess[str]:
+def run(command: List[str]) -> subprocess.CompletedProcess[str]:
     return subprocess.run(command, capture_output=True, text=True)
+
+def sh(command: str) -> subprocess.CompletedProcess[str]:
+    return subprocess.run(command, shell=True)
 
 packages_installed = 0
 aur_packages_installed = 1
-system_services_enabled = 2
-user_services_enabled = 3
+user_services_enabled = 2
+system_services_enabled = 3
 files_contents = 4
 
 operations = {}
 
-def package_exists(package_name: str) -> bool:
-    check_result = sh(['pacman', '-Q', package_name])
-    if check_result.returncode == 0:
-        #log_info(f"Package '{package_name}' is already installed")
+def package_is_installed(package_name: str) -> bool:
+    if 0 == run(['pacman', '-Q', package_name]).returncode:
+        log_info(f"Package '{package_name}' is already installed")
         return True
+    log_warning(f"Package '{package_name}' is not installed")
     return False
 
 def ensure_aur_package_installed_raw(package_name):
     """Ensure some package is installed from AUR assuming yay isn't available"""
-    #check_result = sh(['which', 'yay'])
-    if package_exists(package_name):
+    if package_is_installed(package_name): # run(['which', 'yay'])
         return True
-    log_info(f"Installing {package_name} raw from AUR")
+    log_warning(f"Installing '{package_name}' raw from AUR")
     original_cwd = os.getcwd()
     temp_dir = "/tmp"
     try:
         os.chdir(temp_dir)
         package_dir = Path(temp_dir) / package_name
         if package_dir.exists():
-            sh(['rm', 'rf', str(package_dir)])
-        clone_result = sh(['git', 'clone', f'https://aur.archlinux.org/{package_name}.git'])
-        if clone_result.returncode != 0:
+            run(['rm', 'rf', str(package_dir)])
+        clone_result = run(['git', 'clone', f'https://aur.archlinux.org/{package_name}.git'])
+        if 0 != clone_result.returncode:
             log_error(f"Failed to clone '{package_name}' from AUR repository: {clone_result.stderr}")
             return False
         os.chdir(str(package_dir))
-        makepkg_result = sh(['makepkg', '-si', '--noconfirm'])
-        if makepkg_result.returncode != 0:
+        makepkg_result = run(['makepkg', '-si', '--noconfirm'])
+        if 0 != makepkg_result.returncode:
             log_error(f"Failed to build '{package_name}': {makepkg_result.stderr}")
             return False
         return True
@@ -97,7 +99,7 @@ def ensure_aur_package_installed_raw(package_name):
         os.chdir(original_cwd)
         package_dir = Path(temp_dir) / package_name
         if package_dir.exists():
-            sh(['rm', '-rf', str(package_dir)])
+            run(['rm', '-rf', str(package_dir)])
 
 def ensure(configurations_sequence: List[Dict]) -> bool:
     """Main function to ensure all configurations are applied sequentially"""
@@ -105,18 +107,19 @@ def ensure(configurations_sequence: List[Dict]) -> bool:
     for subject in configurations_sequence:
         result = operations[subject["ensure"]](subject["for"])
         if not result:
-            #log_error(f"ensure(): Failed")
+            log_error("An error occurred during configuration check")
             return False
+    log_success("Configuration check success")
     return True
 
 def ensure_packages_installed(package_list: List[str]) -> bool:
     """Ensure packages are installed using pacman"""
     for package_name in package_list:
-        if package_exists(package_name):
+        if package_is_installed(package_name):
             continue
-        log_info(f"Installing pacman package '{package_name}")
-        install_result = sh(['sudo', 'pacman', '-S', '--noconfirm', package_name])
-        if install_result.returncode != 0:
+        log_info(f"Installing pacman package '{package_name}'")
+        install_result = run(['sudo', 'pacman', '-S', '--noconfirm', package_name])
+        if 0 != install_result.returncode:
             log_error(f"Failed to install pacman package '{package_name}': {install_result.stderr}")
             return False
     return True
@@ -126,68 +129,68 @@ operations[packages_installed] = ensure_packages_installed
 def ensure_aur_packages_installed(package_list: List[str]) -> bool:
     """Ensure AUR packages are installed using yay"""
     for package_name in package_list:
-        if package_exists(package_name):
+        if package_is_installed(package_name):
             continue
-        log_info(f"Installing AUR package '{package_name}")
-        install_result = sh(['yay', '-S', '--noconfirm', package_name])
-        if install_result != 0:
+        log_info(f"Installing AUR package '{package_name}'")
+        install_result = run(['yay', '-S', '--noconfirm', package_name])
+        if 0 != install_result.returncode:
             log_error(f"Failed to install AUR package '{package_name}': {install_result.stderr}")
             return False
     return True
 
 operations[aur_packages_installed] = ensure_aur_packages_installed
 
-def ensure_services_enabled(is_system: bool, service_list: List[str]) -> bool:
+systemctl_prefixes = [
+    [ "systemctl", "--user" ], # is_system = False or 0
+    [ "sudo", "systemctl" ], # is_system = True or 1
+]
+
+def ensure_services_enabled(systemctl_prefix: List[str], service_list: List[str]) -> bool:
     """Ensure services are enabled and started"""
     for service_name in service_list:
-        if not service_name.endswith('.service'):
-            service_name += '.service'
-        enable_result = sh(['systemctl', mode, 'enable', service_name])
-        if enable_result.returncode != 0:
-            log_error(f"Failed to enable service '{service_name}': {enable_result.stderr}")
-            return False
-        status_result = sh(['systemctl', mode, 'is-active', service_name])
-        if status_result.stdout.strip() != 'active':
+        #if not service_name.endswith('.service'):
+        #    service_name += '.service'
+        if 'enabled' != run(systemctl_prefix + ['is-enabled', service_name]).stdout.strip():
+            log_info(f"Enabling service '{service_name}'")
+            enable_result = run(systemctl_prefix + ['enable', service_name])
+            if 0 != enable_result.returncode:
+                log_error(f"Failed to enable service '{service_name}': {enable_result.stderr}")
+                return False
+        if 'active' != run(systemctl_prefix + ['is-active', service_name]).stdout.strip():
             log_info(f"Starting service '{service_name}'")
-            start_result = sh(['systemctl', mode, 'start', service_name])
-            if start_result.returncode != 0:
-                print(f"Failed to start service'{service_name}: {start_result.stderr}")
+            start_result = run(systemctl_prefix + ['start', service_name])
+            if 0 != start_result.returncode:
+                print(f"Failed to start service '{service_name}': {start_result.stderr}")
                 return False
     return True
 
-def ensure_system_services_enabled(service_list: List[str]) -> bool:
-    return ensure_services_enabled("--system", service_list)
-
-operations[system_services_enabled] = ensure_system_services_enabled
-
 def ensure_user_services_enabled(service_list: List[str]) -> bool:
-    return ensure_services_enabled("--user", service_list)
+    return ensure_services_enabled(systemctl_prefixes[0], service_list)
 
 operations[user_services_enabled] = ensure_user_services_enabled
+
+def ensure_system_services_enabled(service_list: List[str]) -> bool:
+    return ensure_services_enabled(systemctl_prefixes[1], service_list)
+
+operations[system_services_enabled] = ensure_system_services_enabled
 
 def ensure_files_contents(file_list):
     for file_config in file_list:
         filename = file_config["filename"]
         contents = file_config["contents"]
-        result = subprocess.call(
-            f"sudo grep -Fq -- {shlex.quote(contents)} {shlex.quote(filename)}",
-            shell=True, )
-        if result == 0:
+        grep_result = sh(
+            f"sudo grep -Fq -- {shlex.quote(contents)} {shlex.quote(filename)}")
+        if 0 == grep_result.returncode:
             continue
         log_info(f"Appending content to '{filename}'")
-        newline_result = subprocess.call(
-                # 0a = newline
-                f"sudo tail -c1 {shlex.quote(filename)} | od -An -t x1 | grep -q '0a'",
-                shell=True, )
-        if newline_result != 0:
+        newline_result = sh( # 0a = newline
+            f"sudo tail -c1 {shlex.quote(filename)} | od -An -t x1 | grep -q '0a'")
+        if 0 != newline_result.returncode:
             contents = "\n" + contents
-        try:
-            subprocess.run(
-                f"sudo echo {shlex.quote(contents)} >> {shlex.quote(filename)}",
-                shell=True,
-                check=True, )
-        except subprocess.CalledProcessError as e:
-            log_error(f"Error writing to '{filename}': {e}")
+        write_result = sh(
+            f"sudo echo {shlex.quote(contents)} >> {shlex.quote(filename)}")
+        if 0 != write_result.returncode:
+            log_error(f"Error writing to '{filename}': {write_result.stderr}")
             return False
     return True
 
