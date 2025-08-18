01c3f03a1583d83493827fa809353610a5df3d4e
kyrylosheliah <kyrylosheliah@gmail.com>
2025-08-18 19:08:18 +0300

    fix and expand line break keymap functionality

--- .config/nvim/lua/base/keymap.lua
+++ .config/nvim/lua/base/keymap.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local set = vim.keymap.set
+local esc_key_code = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+local cr_key_code = vim.api.nvim_replace_termcodes("<CR>", true, false, true)
 
 set("n", "<leader>w", "<cmd>w<cr>", { desc = "Write" })
 set("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
@@ -13,9 +15,37 @@ set("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
 -- `z : go to `z` location
 
 -- the opposite of `<S-j>` or `J` or "join", "break"
-set("n", "<leader>b", "i<CR><ESC>dv0", { desc = "Break the line before the cursor" })
-set("n", "<leader>B", "a<CR><ESC>dv0", { desc = "Break the line after the cursor" })
-set("x", "<leader>b", "\"_di<CR><ESC>dv0", { desc = "Break the line outside selection" })
+local BreakLine = function(args)
+  args = args or {}
+  local after = args.after or false
+  local visual = args.visual or false
+  local indent = args.indent or false
+  if visual then -- delete into the void register
+    vim.api.nvim_feedkeys("\"_d", "n", false)
+  end
+  if after then
+    vim.api.nvim_feedkeys("a", "n", false)
+  else
+    vim.api.nvim_feedkeys("i", "n", false)
+  end
+  vim.api.nvim_feedkeys(cr_key_code, "n", false)
+  vim.api.nvim_feedkeys(esc_key_code, "n", false)
+  if indent then
+    return
+  end
+  vim.api.nvim_feedkeys("d", "n", false)
+  -- if new line cursor is not on the first column, then
+  if after or vim.fn.col(".") ~= 1 then
+    vim.api.nvim_feedkeys("v", "n", false)
+  end
+  vim.api.nvim_feedkeys("0", "n", false)
+end
+set("n", "<leader>b", BreakLine, { desc = "Break the line before the cursor" })
+set("n", "<leader>B", function() BreakLine({ after = true }) end, { desc = "Break the line after the cursor" })
+set("x", "<leader>b", function() BreakLine({ visual = true }) end, { desc = "Break the line outside selection" })
+set("n", "<leader>v", function() BreakLine({ indent = true }) end, { desc = "Break the line before the cursor, preserve indentation" })
+set("n", "<leader>V", function() BreakLine({ indent = true, after = true }) end, { desc = "Break the line after the cursor, preserve indentation" })
+set("x", "<leader>v", function() BreakLine({ indent = true, visual = true }) end, { desc = "Break the line outside selection, preserve indentation" })
 
 -- greatest remap ever indeed
 set("x", "<leader>p", "\"_dP")
@@ -30,7 +60,6 @@ set("x", "<leader>p", "\"_dP")
 set("v", "<", "<gv")
 set("v", ">", ">gv")
 
-local esc_key_code = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
 set({ "i", "n" }, "<esc>", function()
   vim.cmd("nohlsearch") --"<cmd>noh<cr><esc>"
   vim.cmd.redrawstatus()
